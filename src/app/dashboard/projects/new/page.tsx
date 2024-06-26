7cf1dccecf509b31ac5d28d5d7d6d766a4a73a59
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-26 13:47:03 +0800

    refactor: Rename page component to NewProjectPage

--- src/app/dashboard/projects/new/page.tsx
+++ src/app/dashboard/projects/new/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AddProjectForm from "./add-project-form";
 
-export default function page() {
+export default function NewProjectPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 xl:p-4">
       <h1 className="self-start text-3xl font-bold">Add New Project</h1>

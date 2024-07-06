b485e0cdf7e86a0e780a773d9c371aee133ff385
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-07-06 12:55:03 +0800

    refactor: Update modal component classNames for better responsiveness

--- src/app/@modal/(.)projects/[id]/page.tsx
+++ src/app/@modal/(.)projects/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Modal from "@/components/Modal";
 import SourceCodeButton from "@/components/SourceCodeButton";
 import { DialogTitle } from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { getProjectDetail } from "@/server/queries/project-queries";
 import { FolderGit } from "lucide-react";
 import Image from "next/image";
@@ -35,7 +36,9 @@ export default async function ModalDetailProject({
             <FolderGit className="m-auto h-20 w-20" />
           </div>
         )}
-        <p className="text-lg font-medium">{detailProject.description}</p>
+        <ScrollArea className="flex max-h-[650px] w-full flex-col items-center justify-center">
+          <p className="text-lg font-medium">{detailProject.description}</p>
+        </ScrollArea>
         <div className="flex w-full flex-col items-center justify-center gap-5">
           <p className="text-lg font-medium">Tech Stack</p>
           <ul className="flex flex-wrap gap-1">

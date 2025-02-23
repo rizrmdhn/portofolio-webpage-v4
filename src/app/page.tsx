a8b14b6d060352c3e67ac37a05e47e79a73dd286
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-02-23 11:35:32 +0800

    feat: Add link to V2 page in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,6 +117,12 @@ export default async function Home() {
                 >
                   Contact Me
                 </Link>
+                <Link
+                  className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+                  href="/v2"
+                >
+                  V2
+                </Link>
               </div>
             </div>
             <Image
